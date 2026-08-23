38a4be8a99d608c702a89ab0990b6bb9de3b3060
Prashant Bajracharya <royaleplay645@gmail.com>
2026-08-23 14:54:58 +0545

    Configured docker.

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,8 +3,12 @@ import "dotenv/config";
 import { connectDB } from "./lib/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import cors from "cors";
+import fs from "fs";
+import path from "path";
 
 const app = express();
+
+const publicDir = path.join(process.cwd(), "public");
 
 app.use(express.json());
 app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
@@ -13,6 +17,16 @@ app.use(clerkMiddleware());
 app.get("/health", (req, res) => {
   res.status(200).json({ ok: true });
 });
+
+//if the public directory exists, serve the static files
+//this is for the production build
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
+
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
+}
 
 app.listen(process.env.PORT, () => {
   connectDB();
